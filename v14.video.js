f964ec8b4ca80bb871bc7141b016b4a6c1be724d
NiiHen <63357664+NiiHen@users.noreply.github.com>
2021-03-19 06:33:40 +0700

    Update and rename v13.video.js to v14.video.js

--- v14.video.js
+++ v14.video.js
@@ -1,8 +1,3 @@
-
-
-var buatmakan = "https://www.inafut.com/p/in-stream.html";
-var harta = window.location.href;
-  
 //ini script untuk ambil parameter
 function getUrlVars() {
     var vars = {};
@@ -22,10 +17,12 @@ function getUrlParam(parameter, defaultvalue){
 
 var vp1 = getUrlParam('vp1',null);
 var cv1 = getUrlParam('cv1',null);
+var jd1 = getUrlParam('jd1',null);
 
 if(vp1 == null){
-var videoplayer1 = "https://1.bp.blogspot.com/-HlsivgqrRtw/YEmxHKh1XFI/AAAAAAAAASQ/MUkr1bnKbMM18Y9QPQqorB4SIeRUCsv8gCLcBGAsYHQ/s500-rh/Foods.gif";
-var covervideo1 = "https://1.bp.blogspot.com/-HlsivgqrRtw/YEmxHKh1XFI/AAAAAAAAASQ/MUkr1bnKbMM18Y9QPQqorB4SIeRUCsv8gCLcBGAsYHQ/s500-k/Foods.gif";	
+var videoplayer1 = "https://1.bp.blogspot.com/-HlsivgqrRtw/YEmxHKh1XFI/AAAAAAAAASQ/MUkr1bnKbMM18Y9QPQqorB4SIeRUCsv8gCLcBGAsYHQ/s360-rh/Foods.gif";
+var covervideo1 = "https://1.bp.blogspot.com/-HlsivgqrRtw/YEmxHKh1XFI/AAAAAAAAASQ/MUkr1bnKbMM18Y9QPQqorB4SIeRUCsv8gCLcBGAsYHQ/s360-k/Foods.gif";	
+var defaultjudul = "How to apply the white sauce";
 var scdef = "<source src='"+videoplayer1+"' type='video/mp4'/>";
 $(document).ready(function(){
 	$('#my-video').each(function(){$(this).attr("poster", covervideo1); });
@@ -35,6 +32,7 @@ $(document).ready(function(){
 	
 var encryptvp1 = decodeURIComponent(vp1);
 var encryptcv1 = decodeURIComponent(cv1);
+var encryptjd1 = decodeURIComponent(jd1);
 var scawal = "<source src='"+encryptvp1+"' type='video/mp4'/>";
 $(document).ready(function(){
 	$('#my-video').each(function(){$(this).attr("poster", encryptcv1); });
